47b2d3c9899dc4fe4ac00222af44a49923265201
atharvadpatil <atharva1635@gmail.com>
2026-04-09 15:51:58 -0400

    Add AuthContext + SignInPage + ProtectedRoute (Closes #4)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import SignInPage from './pages/SignInPage'
+import DashboardPage from './pages/DashboardPage'
+import ProtectedRoute from './components/ProtectedRoute'
+import DashboardLayout from './layouts/DashboardLayout'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/dashboard/:appId" element={<DashboardPage />} />
+        </Route>
+      </Route>
     </Routes>
   )
 }
