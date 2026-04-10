e04fb45162263b9fbe72f11e5292d715c8e8c08d
Pranjeet Dhanapune <pranjeetdhanapune@gmail.com>
2026-04-10 13:20:01 -0400

    Resolved few issues (closes #12)

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,25 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ChatWindow from '../components/ChatWindow'
 import VisualizationView from '../components/VisualizationView'
 import ViewToggle from '../components/ViewToggle'
 
-export default function DashboardPage() {
-  const { appId } = useParams()
+function AppView() {
   const [activeView, setActiveView] = useState('chat')
-
-  // FR-12.3: reset to Chat when navigating between apps
-  useEffect(() => {
-    setActiveView('chat')
-  }, [appId])
-
-  if (!appId) {
-    return (
-      <div className="flex h-full items-center justify-center">
-        <p className="text-gray-500 text-sm">Select or create an app to get started.</p>
-      </div>
-    )
-  }
 
   return (
     <div className="flex flex-col h-full">
@@ -29,4 +15,18 @@ export default function DashboardPage() {
       </div>
     </div>
   )
+}
+
+export default function DashboardPage() {
+  const { appId } = useParams()
+
+  if (!appId) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <p className="text-gray-500 text-sm">Select or create an app to get started.</p>
+      </div>
+    )
+  }
+
+  return <AppView key={appId} />
 }
